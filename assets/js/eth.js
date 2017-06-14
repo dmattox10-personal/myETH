87df0c35f1f5f7ca4c4e03dc54126cfbf35c1907
Daniel Mattox <dmattox10@gmail.com>
2017-06-13 22:20:42 -0400

    finally commented some of my code

--- assets/js/eth.js
+++ assets/js/eth.js
@@ -18,7 +18,7 @@ function requests() {
     if (balanceRequest.status >= 200 && balanceRequest.status < 400) {
       var input = JSON.parse(balanceRequest.responseText);
       var balance = input.data;
-      updateBalance(balance); //THIS IS THE TYPE OF FUNCTION WE WILL CALL TO UPDATE EACH AND EVERY ONE
+      updateBalance(balance);
     }
     else {
       console.log("We connected to the server, but it returned an error.");
@@ -31,15 +31,15 @@ function requests() {
       var input = JSON.parse(xcRequest.responseText);
       var BTC = input[0].price_btc;
       var USD = input[0].price_usd;
-      updateXC(USD, BTC); //THIS IS THE TYPE OF FUNCTION WE WILL CALL TO UPDATE EACH AND EVERY ONE
+      updateXC(USD, BTC);
     }
     else {
       console.log("We connected to the server, but it returned an error.");
     }
   }
-  getHashrate(hashrateURL);
+  getHashrate(hashrateURL); //At this point, I couldn't get the next block of request code to fire, so I turned it into a function
 }
-function getHashrate(URL) {
+function getHashrate(URL) { //The function referenced above is defined here
   var hashrateRequest = new XMLHttpRequest();
   hashrateRequest.open('GET', URL);
   hashrateRequest.send();
@@ -47,14 +47,14 @@ function getHashrate(URL) {
     if (hashrateRequest.status >= 200 && hashrateRequest.status < 400) {
       var input = JSON.parse(hashrateRequest.responseText);
       var hashrate = input.data.h24;
-      getProjections(hashrate);
+      getProjections(hashrate); // Same problem, had to call a function rather than continue the natural progression
     }
     else {
       console.log("We connected to the server, but it returned an error.");
     }
   }
 }
-function getProjections (hashrate) {
+function getProjections (hashrate) { // Function gets called
   hashrate = JSON.stringify(hashrate);
   projectionsURL = projectionsURL.concat(hashrate);
   var projectionsRequest = new XMLHttpRequest();
@@ -66,14 +66,14 @@ function getProjections (hashrate) {
       var day = input.data.day.dollars;
       var week = input.data.week.dollars;
       var month = input.data.month.dollars;
-      updateProjections(day, week, month);
+      updateProjections(day, week, month); //Calling the next function to continue
     }
     else {
       console.log("We connected to the server, but it returned an error.");
     }
   }
 }
-function getWallet() {
+function getWallet() { //Still following the daisy chain of functions
   var walletRequest = new XMLHttpRequest();
   walletRequest.open('GET', walletURL);
   walletRequest.send();
@@ -81,7 +81,7 @@ function getWallet() {
     if (walletRequest.status >= 200 && walletRequest.status < 400) {
       var input = JSON.parse(walletRequest.responseText);
       var walletContents = input.result;
-      updateWallet(walletContents);
+      updateWallet(walletContents); // You;ll never stop me!
     }
     else {
       console.log("We connected to the server, but it returned an error.");
@@ -98,13 +98,13 @@ function updateProjections(day, week, month) {
   document.getElementById("Day").innerHTML = "$" + day.toFixed(2);
   document.getElementById("Week").innerHTML = "$" + week.toFixed(2);
   document.getElementById("Month").innerHTML = "$" + month.toFixed(2);
-  getWallet();
+  getWallet(); //There really has to be a way to shorten the functions that make the calls
 }
 function updateXC(USD, BTC) {
   document.getElementById("BTC").innerHTML=BTC;
   document.getElementById("USD").innerHTML=USD;
 }
-function updateWallet(value) {
+function updateWallet(value) { //This is where the program finally finishes
   document.getElementById("Wallet").innerHTML=value;
 
 }
