89ffd14d19c14ac525894d9d781e3c4cc517059c
Daniel Mattox <dmattox10@gmail.com>
2017-06-16 13:10:43 -0400

    Using my address until the RIG is live for all three

--- assets/js/eth.js
+++ assets/js/eth.js
@@ -1,18 +1,31 @@
-var ethAddress = "";
-var balanceURL = "https://api.nanopool.org/v1/eth/balance/" + ethAddress;
-var projectionsURL = "https://api.nanopool.org/v1/eth/approximated_earnings/";
-var exchangeURL = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=BTC";
-var walletURL = "https://api.etherscan.io/api?module=account&action=balance&address=" + ethAddress + "&tag=latest&apikey=PGNUMCQNNJ88US2ITXIHV3NZ38F7TJR5PV"
-
-function requests(ethAddressIn) {
-  ethAddress = ethAddressIn;
-  var balanceRequest = new XMLHttpRequest();
-  var projectionsRequest = new XMLHttpRequest();
-  var xcRequest = new XMLHttpRequest();
-
+function requests(choice) {
+  var ethAddress = "";
+  switch(choice) {
+    case 0:
+    ethAddress = "0x507f3b2028ec5d7039526915efd2fa75c6567731"; //Using my address for all three, until the Rig goes live
+    break;
+    case 1:
+    ethAddress = "0x507f3b2028ec5d7039526915efd2fa75c6567731";
+    //ethAddress = "0x3677666BA14809229e28baC92bFBB78cf6d447bf";
+    break;
+    case 2:
+    ethAddress = "0x507f3b2028ec5d7039526915efd2fa75c6567731";
+    //ethAddress = "0xB70D62c3034b404c3e22B59233952578a8F34006";
+    break;
+  }
   var balanceURL = "https://api.nanopool.org/v1/eth/balance/" + ethAddress;
+  var walletURL = "https://api.etherscan.io/api?module=account&action=balance&address=" + ethAddress + "&tag=latest&apikey=PGNUMCQNNJ88US2ITXIHV3NZ38F7TJR5PV"
   var hashrateURL = "https://api.nanopool.org/v1/eth/avghashrate/" + ethAddress;
+  var exchangeURL = "https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=BTC";
 
+  getBalance(balanceURL);
+  getWallet(walletURL);
+  getHashrate(hashrateURL);
+  getExchange(exchangeURL);
+}
+
+function getBalance(balanceURL) {
+  var balanceRequest = new XMLHttpRequest();
   balanceRequest.open('GET', balanceURL);
   balanceRequest.send();
   balanceRequest.onload = function() {
@@ -25,24 +38,31 @@ function requests(ethAddressIn) {
       console.log("We connected to the server, but it returned an error.");
     }
   }
-  xcRequest.open('GET', exchangeURL);
-  xcRequest.send();
-  xcRequest.onload = function() {
-    if (xcRequest.status >= 200 && xcRequest.status < 400) {
-      var input = JSON.parse(xcRequest.responseText);
-      var BTC = input[0].price_btc;
-      var USD = input[0].price_usd;
-      updateXC(USD, BTC);
+}
+function updateBalance(balance) {
+  document.getElementById("Balance").innerHTML = balance;
+}
+function getWallet(walletURL) { //Still following the daisy chain of functions
+  var walletRequest = new XMLHttpRequest();
+  walletRequest.open('GET', walletURL);
+  walletRequest.send();
+  walletRequest.onload = function() {
+    if (walletRequest.status >= 200 && walletRequest.status < 400) {
+      var input = JSON.parse(walletRequest.responseText);
+      var walletContents = input.result;
+      updateWallet(walletContents); // You;ll never stop me!
     }
     else {
       console.log("We connected to the server, but it returned an error.");
     }
   }
-  getHashrate(hashrateURL); //At this point, I couldn't get the next block of request code to fire, so I turned it into a function
 }
-function getHashrate(URL) { //The function referenced above is defined here
+function updateWallet(walletContents) { //This is where the program finally finishes
+  document.getElementById("Wallet").innerHTML=walletContents;
+}
+function getHashrate(hashrateURL) { //The function referenced above is defined here
   var hashrateRequest = new XMLHttpRequest();
-  hashrateRequest.open('GET', URL);
+  hashrateRequest.open('GET', hashrateURL);
   hashrateRequest.send();
   hashrateRequest.onload = function() {
     if (hashrateRequest.status >= 200 && hashrateRequest.status < 400) {
@@ -55,8 +75,9 @@ function getHashrate(URL) { //The function referenced above is defined here
     }
   }
 }
-function getProjections (hashrate) { // Function gets called
+function getProjections(hashrate) { // Function gets called
   hashrate = JSON.stringify(hashrate);
+  var projectionsURL = "https://api.nanopool.org/v1/eth/approximated_earnings/";
   projectionsURL = projectionsURL.concat(hashrate);
   var projectionsRequest = new XMLHttpRequest();
   projectionsRequest.open('GET', projectionsURL);
@@ -74,38 +95,28 @@ function getProjections (hashrate) { // Function gets called
     }
   }
 }
-function getWallet() { //Still following the daisy chain of functions
-  var walletRequest = new XMLHttpRequest();
-  walletRequest.open('GET', walletURL);
-  walletRequest.send();
-  walletRequest.onload = function() {
-    if (walletRequest.status >= 200 && walletRequest.status < 400) {
-      var input = JSON.parse(walletRequest.responseText);
-      var walletContents = input.result;
-      updateWallet(walletContents); // You;ll never stop me!
+function updateProjections(day, week, month) {
+  document.getElementById("Day").innerHTML = "$" + day.toFixed(2);
+  document.getElementById("Week").innerHTML = "$" + week.toFixed(2);
+  document.getElementById("Month").innerHTML = "$" + month.toFixed(2);
+}
+function getExchange(exchangeURL) {
+  var xcRequest = new XMLHttpRequest();
+  xcRequest.open('GET', exchangeURL);
+  xcRequest.send();
+  xcRequest.onload = function() {
+    if (xcRequest.status >= 200 && xcRequest.status < 400) {
+      var input = JSON.parse(xcRequest.responseText);
+      var BTC = input[0].price_btc;
+      var USD = input[0].price_usd;
+      updateXC(USD, BTC);
     }
     else {
       console.log("We connected to the server, but it returned an error.");
     }
   }
 }
-function updateBalance(balance) {
-  document.getElementById("Balance").innerHTML = balance;
-}
-function updateHashrate(hashrate) {
-  globalHashrate = JSON.stringify(hashrate);
-}
-function updateProjections(day, week, month) {
-  document.getElementById("Day").innerHTML = "$" + day.toFixed(2);
-  document.getElementById("Week").innerHTML = "$" + week.toFixed(2);
-  document.getElementById("Month").innerHTML = "$" + month.toFixed(2);
-  getWallet(); //There really has to be a way to shorten the functions that make the calls
-}
 function updateXC(USD, BTC) {
   document.getElementById("BTC").innerHTML=BTC;
   document.getElementById("USD").innerHTML=USD;
-}
-function updateWallet(value) { //This is where the program finally finishes
-  document.getElementById("Wallet").innerHTML=value;
-
 }
